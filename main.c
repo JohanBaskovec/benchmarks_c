35e3f976be7d09189deb5f84c5e2a7bd79aa3d8a
Johan Baskovec <johan@johanbaskovec.fr>
2020-04-06 10:28:10 +0200

    add tests for matrix multiplication

--- main.c
+++ main.c
@@ -85,80 +85,239 @@ vec3CrossProductPP(Vec3 *t, Vec3 const *a, Vec3 const *b) {
     t->z = a->x * b->y - a->y * b->x;
 }
 
+typedef struct Mat4f {
+    // column 0
+    float m00;
+    float m01;
+    float m02;
+    float m03;
 
-#define BENCH_VEC_N 5000000
+    // column 1
+    float m10;
+    float m11;
+    float m12;
+    float m13;
+
+    // column 2
+    float m20;
+    float m21;
+    float m22;
+    float m23;
+
+    // column 3
+    float m30;
+    float m31;
+    float m32;
+    float m33;
+} Mat4f;
+
+Mat4f
+mat4fMul(Mat4f a, Mat4f b) {
+    Mat4f ret;
+    ret.m00 = a.m00*b.m00 + a.m10*b.m01 + a.m20*b.m02 + a.m30*b.m03;
+    ret.m01 = a.m00*b.m10 + a.m10*b.m11 + a.m20*b.m12 + a.m30*b.m13;
+    ret.m02 = a.m00*b.m20 + a.m10*b.m21 + a.m20*b.m22 + a.m30*b.m23;
+    ret.m03 = a.m00*b.m30 + a.m10*b.m31 + a.m20*b.m32 + a.m30*b.m33;
+
+    ret.m10 = a.m01*b.m00 + a.m11*b.m01 + a.m21*b.m02 + a.m31*b.m03;
+    ret.m11 = a.m01*b.m10 + a.m11*b.m11 + a.m21*b.m12 + a.m31*b.m13;
+    ret.m12 = a.m01*b.m20 + a.m11*b.m21 + a.m21*b.m22 + a.m31*b.m23;
+    ret.m13 = a.m01*b.m30 + a.m11*b.m31 + a.m21*b.m32 + a.m31*b.m33;
+
+    ret.m20 = a.m02*b.m00 + a.m12*b.m01 + a.m22*b.m02 + a.m32*b.m03;
+    ret.m21 = a.m02*b.m10 + a.m12*b.m11 + a.m22*b.m12 + a.m32*b.m13;
+    ret.m22 = a.m02*b.m20 + a.m12*b.m21 + a.m22*b.m22 + a.m32*b.m23;
+    ret.m23 = a.m02*b.m30 + a.m12*b.m31 + a.m22*b.m32 + a.m32*b.m33;
+
+    ret.m30 = a.m03*b.m00 + a.m13*b.m01 + a.m23*b.m02 + a.m33*b.m03;
+    ret.m31 = a.m03*b.m10 + a.m13*b.m11 + a.m23*b.m12 + a.m33*b.m13;
+    ret.m32 = a.m03*b.m20 + a.m13*b.m21 + a.m23*b.m22 + a.m33*b.m23;
+    ret.m33 = a.m03*b.m30 + a.m13*b.m31 + a.m23*b.m32 + a.m33*b.m33;
+    return ret;
+}
+
+Mat4f
+mat4fMulP(Mat4f const *a, Mat4f const *b) {
+    Mat4f ret;
+    ret.m00 = a->m00*b->m00 + a->m10*b->m01 + a->m20*b->m02 + a->m30*b->m03;
+    ret.m01 = a->m00*b->m10 + a->m10*b->m11 + a->m20*b->m12 + a->m30*b->m13;
+    ret.m02 = a->m00*b->m20 + a->m10*b->m21 + a->m20*b->m22 + a->m30*b->m23;
+    ret.m03 = a->m00*b->m30 + a->m10*b->m31 + a->m20*b->m32 + a->m30*b->m33;
+
+    ret.m10 = a->m01*b->m00 + a->m11*b->m01 + a->m21*b->m02 + a->m31*b->m03;
+    ret.m11 = a->m01*b->m10 + a->m11*b->m11 + a->m21*b->m12 + a->m31*b->m13;
+    ret.m12 = a->m01*b->m20 + a->m11*b->m21 + a->m21*b->m22 + a->m31*b->m23;
+    ret.m13 = a->m01*b->m30 + a->m11*b->m31 + a->m21*b->m32 + a->m31*b->m33;
+
+    ret.m20 = a->m02*b->m00 + a->m12*b->m01 + a->m22*b->m02 + a->m32*b->m03;
+    ret.m21 = a->m02*b->m10 + a->m12*b->m11 + a->m22*b->m12 + a->m32*b->m13;
+    ret.m22 = a->m02*b->m20 + a->m12*b->m21 + a->m22*b->m22 + a->m32*b->m23;
+    ret.m23 = a->m02*b->m30 + a->m12*b->m31 + a->m22*b->m32 + a->m32*b->m33;
+
+    ret.m30 = a->m03*b->m00 + a->m13*b->m01 + a->m23*b->m02 + a->m33*b->m03;
+    ret.m31 = a->m03*b->m10 + a->m13*b->m11 + a->m23*b->m12 + a->m33*b->m13;
+    ret.m32 = a->m03*b->m20 + a->m13*b->m21 + a->m23*b->m22 + a->m33*b->m23;
+    ret.m33 = a->m03*b->m30 + a->m13*b->m31 + a->m23*b->m32 + a->m33*b->m33;
+    return ret;
+}
+
+void
+mat4fMulPP(Mat4f *t, Mat4f const *a, Mat4f const *b) {
+    t->m00 = a->m00*b->m00 + a->m10*b->m01 + a->m20*b->m02 + a->m30*b->m03;
+    t->m01 = a->m00*b->m10 + a->m10*b->m11 + a->m20*b->m12 + a->m30*b->m13;
+    t->m02 = a->m00*b->m20 + a->m10*b->m21 + a->m20*b->m22 + a->m30*b->m23;
+    t->m03 = a->m00*b->m30 + a->m10*b->m31 + a->m20*b->m32 + a->m30*b->m33;
+
+    t->m10 = a->m01*b->m00 + a->m11*b->m01 + a->m21*b->m02 + a->m31*b->m03;
+    t->m11 = a->m01*b->m10 + a->m11*b->m11 + a->m21*b->m12 + a->m31*b->m13;
+    t->m12 = a->m01*b->m20 + a->m11*b->m21 + a->m21*b->m22 + a->m31*b->m23;
+    t->m13 = a->m01*b->m30 + a->m11*b->m31 + a->m21*b->m32 + a->m31*b->m33;
+
+    t->m20 = a->m02*b->m00 + a->m12*b->m01 + a->m22*b->m02 + a->m32*b->m03;
+    t->m21 = a->m02*b->m10 + a->m12*b->m11 + a->m22*b->m12 + a->m32*b->m13;
+    t->m22 = a->m02*b->m20 + a->m12*b->m21 + a->m22*b->m22 + a->m32*b->m23;
+    t->m23 = a->m02*b->m30 + a->m12*b->m31 + a->m22*b->m32 + a->m32*b->m33;
+
+    t->m30 = a->m03*b->m00 + a->m13*b->m01 + a->m23*b->m02 + a->m33*b->m03;
+    t->m31 = a->m03*b->m10 + a->m13*b->m11 + a->m23*b->m12 + a->m33*b->m13;
+    t->m32 = a->m03*b->m20 + a->m13*b->m21 + a->m23*b->m22 + a->m33*b->m23;
+    t->m33 = a->m03*b->m30 + a->m13*b->m31 + a->m23*b->m32 + a->m33*b->m33;
+}
+
+#define BENCH_VEC_N 1000000
+#define BENCH_MAT_N 1000000
 #define BENCH_BENCHMARKS_N 5
 
-Vec3 *data;
-Vec3 *dataV;
-Vec3 *dataP;
-Vec3 *dataPP;
+Vec3 *dataVec;
+Vec3 *dataVecV;
+Vec3 *dataVecP;
+Vec3 *dataVecPP;
 
-float vTime = 0;
-float pTime = 0;
-float ppTime = 0;
+Mat4f *dataMat;
+Mat4f *dataMatV;
+Mat4f *dataMatP;
+Mat4f *dataMatPP;
+
+float vTimeVec = 0;
+float pTimeVec = 0;
+float ppTimeVec = 0;
+
+float vTimeMat = 0;
+float pTimeMat = 0;
+float ppTimeMat = 0;
 
 void runBenchmarks() {
     float startTime;
     float endTime;
     float timeElapsed;
 
+    // VECTORS
     // test passing by value
     startTime = (float)clock()/CLOCKS_PER_SEC;
     for (int i = 0 ; i < BENCH_VEC_N - 1 ; i++) {
-        dataV[i] = vec3Normalize(vec3CrossProduct(data[i], data[i+1]));
+        dataVecV[i] = vec3Normalize(vec3CrossProduct(dataVec[i], dataVec[i+1]));
     }
     endTime = (float)clock()/CLOCKS_PER_SEC;
     timeElapsed = endTime - startTime;
-    vTime += timeElapsed;
-    printf("Passing by value: %f\n", timeElapsed);
+    vTimeVec += timeElapsed;
+    printf("Vector: Passing by value: %f\n", timeElapsed);
 
     // test passing parameters by pointer
     startTime = (float)clock()/CLOCKS_PER_SEC;
     for (int i = 0 ; i < BENCH_VEC_N - 1 ; i++) {
-        dataP[i] = vec3CrossProductP(&data[i], &data[i+1]);
-        dataP[i] = vec3NormalizeP(&dataP[i]);
+        dataVecP[i] = vec3CrossProductP(&dataVec[i], &dataVec[i+1]);
+        dataVecP[i] = vec3NormalizeP(&dataVecP[i]);
     }
     endTime = (float)clock()/CLOCKS_PER_SEC;
     timeElapsed = endTime - startTime;
-    pTime += timeElapsed;
-    printf("Passing by pointer: %f\n", timeElapsed);
+    pTimeVec += timeElapsed;
+    printf("Vector: Passing by pointer: %f\n", timeElapsed);
 
     // test passing parameters and return value by pointer
     startTime = (float)clock()/CLOCKS_PER_SEC;
     for (int i = 0 ; i < BENCH_VEC_N - 1 ; i++) {
-        vec3CrossProductPP(&dataPP[i], &data[i], &data[i+1]);
-        vec3NormalizePP(&dataPP[i], &dataPP[i]);
+        vec3CrossProductPP(&dataVecPP[i], &dataVec[i], &dataVec[i+1]);
+        vec3NormalizePP(&dataVecPP[i], &dataVecPP[i]);
     }
     endTime = (float)clock()/CLOCKS_PER_SEC;
     timeElapsed = endTime - startTime;
-    ppTime += timeElapsed;
-    printf("Passing parameters and return value by pointer: %f\n", timeElapsed);
+    ppTimeVec += timeElapsed;
+    printf("Vector: Passing parameters and return value by pointer: %f\n", timeElapsed);
 
     // prevent optimization by printing a random value from the data
     int rR = rand() % BENCH_VEC_N;
-    printf("These values should be equal: %f %f %f\n", dataV[rR].x, dataP[rR].x, dataPP[rR].x);
+    printf("These values should be equal: %f %f %f\n\n", dataVecV[rR].x, dataVecP[rR].x, dataVecPP[rR].x);
+
+    // MATRICES
+    // test passing by value
+    startTime = (float)clock()/CLOCKS_PER_SEC;
+    for (int i = 0 ; i < BENCH_MAT_N - 1 ; i++) {
+        dataMatV[i] = mat4fMul(dataMat[i], dataMat[i+1]);
+    }
+    endTime = (float)clock()/CLOCKS_PER_SEC;
+    timeElapsed = endTime - startTime;
+    vTimeMat += timeElapsed;
+    printf("Matrix: Passing by value: %f\n", timeElapsed);
+
+    // test passing parameters by pointer
+    startTime = (float)clock()/CLOCKS_PER_SEC;
+    for (int i = 0 ; i < BENCH_MAT_N - 1 ; i++) {
+        dataMatP[i] = mat4fMulP(&dataMat[i], &dataMat[i+1]);
+    }
+    endTime = (float)clock()/CLOCKS_PER_SEC;
+    timeElapsed = endTime - startTime;
+    pTimeMat += timeElapsed;
+    printf("Matrix: Passing by pointer: %f\n", timeElapsed);
+
+    // test passing parameters and return value by pointer
+    startTime = (float)clock()/CLOCKS_PER_SEC;
+    for (int i = 0 ; i < BENCH_MAT_N - 1 ; i++) {
+        mat4fMulPP(&dataMatPP[i], &dataMat[i], &dataMat[i+1]);
+    }
+    endTime = (float)clock()/CLOCKS_PER_SEC;
+    timeElapsed = endTime - startTime;
+    ppTimeMat += timeElapsed;
+    printf("Matrix: Passing parameters and return value by pointer: %f\n", timeElapsed);
+
+    // prevent optimization by printing a random value from the data
+    rR = rand() % BENCH_MAT_N;
+    printf("These values should be equal: %f %f %f\n", dataMatV[rR].m00, dataMatP[rR].m00, dataMatPP[rR].m00);
 }
 
 int main() {
-    data = malloc(BENCH_VEC_N * sizeof(Vec3));
-    dataV = malloc(BENCH_VEC_N * sizeof(Vec3));
-    dataP = malloc(BENCH_VEC_N * sizeof(Vec3));
-    dataPP = malloc(BENCH_VEC_N * sizeof(Vec3));
+    dataVec = malloc(BENCH_VEC_N * sizeof(Vec3));
+    dataVecV = malloc(BENCH_VEC_N * sizeof(Vec3));
+    dataVecP = malloc(BENCH_VEC_N * sizeof(Vec3));
+    dataVecPP = malloc(BENCH_VEC_N * sizeof(Vec3));
+
+    dataMat = malloc(BENCH_MAT_N * sizeof(Mat4f));
+    dataMatV = malloc(BENCH_MAT_N * sizeof(Mat4f));
+    dataMatP = malloc(BENCH_MAT_N * sizeof(Mat4f));
+    dataMatPP = malloc(BENCH_MAT_N * sizeof(Mat4f));
 
     for (int i = 0 ; i < BENCH_VEC_N ; i++) {
-        data[i].x = rand();
-        data[i].y = rand();
-        data[i].z = rand();
+        dataVec[i].x = rand();
+        dataVec[i].y = rand();
+        dataVec[i].z = rand();
+    }
+    for (int i = 0 ; i < BENCH_MAT_N ; i++) {
+        dataMat[i].m00 = rand();
+        dataMat[i].m11 = rand();
+        dataMat[i].m32 = rand();
     }
 
     for (int i = 0 ; i < BENCH_BENCHMARKS_N ; i++) {
         runBenchmarks();
     }
 
-    printf("Average time:\n");
-    printf(" - Parameters by value, return by value: %f\n", vTime / BENCH_BENCHMARKS_N);
-    printf(" - Parameters by pointer, return by value: %f\n", pTime / BENCH_BENCHMARKS_N);
-    printf(" - Parameters by pointer, return by pointer: %f\n", ppTime / BENCH_BENCHMARKS_N);
+    printf("Vec average time:\n");
+    printf(" - Parameters by value, return by value: %f\n", vTimeVec / BENCH_BENCHMARKS_N);
+    printf(" - Parameters by pointer, return by value: %f\n", pTimeVec / BENCH_BENCHMARKS_N);
+    printf(" - Parameters by pointer, return by pointer: %f\n", ppTimeVec / BENCH_BENCHMARKS_N);
+
+    printf("Mat average time:\n");
+    printf(" - Parameters by value, return by value: %f\n", vTimeMat / BENCH_BENCHMARKS_N);
+    printf(" - Parameters by pointer, return by value: %f\n", pTimeMat / BENCH_BENCHMARKS_N);
+    printf(" - Parameters by pointer, return by pointer: %f\n", ppTimeMat / BENCH_BENCHMARKS_N);
 
 }
